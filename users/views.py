f231364ecb7d424c55d67a02cdf9c425c6d757a9
clevrf0x <favasm72@gmail.com>
2023-08-01 00:39:42 +0530

    Added Login and Logout Views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.views import View
 
-# Create your views here.
+
+# TODO: Handle case where users are already authenticated
+class LoginView(View):
+    def get(self, request):
+        return render(request, "users/login.html")
+
+    def post(self, request):
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        if not (username and password):
+            messages.error(request, "Username or password missing")
+            return redirect("login")
+
+        user = authenticate(username=username, password=password)
+
+        if not user:
+            messages.error(request, "Invalid Credentials")
+            return redirect("login")
+
+        login(request, user)
+        messages.info(request, "Login Successful")
+        return redirect("/")
+
+
+class LogoutView(View):
+    def get(self, request):
+        messages.info(request, "Logout Successful")
+        logout(request)
+        return redirect("login")
